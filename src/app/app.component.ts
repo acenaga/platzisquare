73e8cd473aba7636b6ebeacb9040077b92ea3d9d
acenaga <mundofuturoca@gmail.com>
2019-12-29 12:11:26 -0300

    actualizando la carpeta del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platzisquare';
+  autor = 'Carlos Ferrer';
+  company = 'Mundo Futuro';
+  a = 8;
+  b = 9;
+  listo = false;
+  nombre = '';
+  apellido = '';
+  lugares = [
+    {plan:'pagado', cercania: 1, distancia: 1, active: true, nombre: 'Floreria la Gardenia'},
+    {plan:'gratuito', cercania: 1, distancia: 1.8, active: true, nombre: 'Donas La Pasadita'},
+    {plan:'gratuito', cercania: 2, distancia: 5, active: false, nombre: 'Veterinaria Huellitas Felices'},
+    {plan:'gratuito', cercania: 3, distancia: 10, active: true, nombre: 'Sushi sushiroll'},
+    {plan:'pagado', cercania: 3, distancia: 35, active: true, nombre: 'hotel la gracia'},
+    {plan:'gratuito', cercania: 3, distancia: 120, active: false, nombre: 'zapateria el clavo'}
+  ];
+  lat = -34.6368443;
+  lng = -58.5289941;
+
+  personas =[
+    {edad: 33, nombre: 'esteban de jesus'},
+    {edad: 16, nombre: 'Caridad Canelon'},
+    {edad: 25, nombre: 'De la rua '},
+    {edad: 12, nombre: 'Mardita Kichsner'},
+    {edad: 18, nombre: 'Alan Margarito'},
+    {edad: 20, nombre: 'Cualquier verga'},
+    {edad: 13, nombre: 'Me ladille de nombres'},
+  ]
+
+  constructor() {
+    setTimeout(() => {
+      this.listo = true;
+    }, 3000 );
+  }
+
+  hacerAlgo() {
+    alert('Haciendo Algo');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
+import {ResaltarDirective} from './directives/resaltar.directive';
+import {ContarClicksDirective} from './directives/contar-clicks.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResaltarDirective,
+    ContarClicksDirective,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCCKVTy2r9NGPJWeJ0E9Jtb_T5C8jb2Jno'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
